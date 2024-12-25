7d7d87aaf61a2a7fb0dbe05c1d40efdcffa9c852
vishnu2419 <vishnu.jerry@gmail.com>
2024-12-24 23:06:43 -0700

    Update pattern.sh

--- pattern.sh
+++ pattern.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+echo Hi.... Enter a number for number of lines in the pattern.
+read n
+for ((i=0; i<n; i++))
+do
+    for ((j=0; j<i+1; j+=1))
+    do
+    echo -n "*"
+    done
+echo
+done
